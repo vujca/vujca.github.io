7752d6a5dcf781e1043eb1b58eeea881198172ae
vujca <tomeknomek@gmail.com>
2023-08-29 15:39:58 +0200

    Test clipboard new

--- assets/js/copyCode.js
+++ assets/js/copyCode.js
@@ -1,5 +1,33 @@
+// This assumes that you're using Rouge; if not, update the selector
+/*const codeBlocks = document.querySelectorAll('.code-header + .highlighter-rouge');
+const copyCodeButtons = document.querySelectorAll('.copy-code-button');
+
+copyCodeButtons.forEach((copyCodeButton, index) => {
+  const code = codeBlocks[index].innerText;
+
+  copyCodeButton.addEventListener('click', () => {
+    // Copy the code to the user's clipboard
+    
+
+    // Update the button text visually
+    const { innerText: originalText } = copyCodeButton;
+    copyCodeButton.innerText = 'Copied!';
+
+    // (Optional) Toggle a class for styling the button
+    copyCodeButton.classList.add('copied');
+
+    // After 2 seconds, reset the button to its initial UI
+    setTimeout(() => {
+      copyCodeButton.innerText = originalText;
+      copyCodeButton.classList.remove('copied');
+    }, 2000);
+  });
+});*/
+
+const codeBlocks = document.querySelectorAll('.code-text + .highlighter-rouge');
 let copyText = document.querySelector(".copy-text");
 copyText.querySelector("button").addEventListener("click", function () {
+    const code = codeBlocks[index].innerText;
 	window.navigator.clipboard.writeText(code);
 	document.execCommand("copy");
 	copyText.classList.add("active");
